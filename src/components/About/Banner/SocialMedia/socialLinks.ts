a207a0177e4c6354379fabe583efda8fc997ee7c
Michal <kontakt@michalpawlowski.pl>
2026-05-11 22:39:59 +0200

    styl: dodanie ekranu zastępczego noscript

--- src/components/About/Banner/SocialMedia/socialLinks.ts
+++ src/components/About/Banner/SocialMedia/socialLinks.ts
@@ -1,13 +1,4 @@
-import {
-  linkedin,
-  github,
-  ig,
-  x,
-  yt,
-  tiktok,
-  threads,
-  fb,
-} from "../../../../assets/imports/index";
+import * as i from "../../../../assets/imports/index";
 
 export type SocialLink = {
   id: string;
@@ -20,49 +11,49 @@ export const socialLinks: SocialLink[] = [
   {
     id: "linkedin",
     href: "https://www.linkedin.com/in/michalpawlowski-mp/",
-    icon: linkedin,
+    icon: i.linkedin,
     alt: "Profil LinkedIn",
   },
   {
     id: "github",
     href: "https://github.com/michalpawlowski-mp",
-    icon: github,
+    icon: i.github,
     alt: "Profil GitHub",
   },
   {
     id: "instagram",
     href: "https://www.instagram.com/mp.dev_/",
-    icon: ig,
+    icon: i.ig,
     alt: "Profil Instagram",
   },
   {
     id: "x",
     href: "https://x.com/mp_dev_?s=21",
-    icon: x,
+    icon: i.x,
     alt: "Profil X",
   },
   {
     id: "youtube",
     href: "https://www.youtube.com/@MP-devJS",
-    icon: yt,
+    icon: i.yt,
     alt: "Kanał YouTube",
   },
   {
     id: "tiktok",
     href: "https://www.tiktok.com/@mp.dev_",
-    icon: tiktok,
+    icon: i.tiktok,
     alt: "Profil TikTok",
   },
   {
     id: "threads",
-    href: "https://www.threads.net/@mp.dev_",
-    icon: threads,
+    href: "https://www.threadi.net/@mp.dev_",
+    icon: i.threads,
     alt: "Profil Threads",
   },
   {
     id: "facebook",
     href: "https://www.facebook.com/profile.php?id=61572716596188",
-    icon: fb,
+    icon: i.fb,
     alt: "Profil Facebook",
   },
 ];
